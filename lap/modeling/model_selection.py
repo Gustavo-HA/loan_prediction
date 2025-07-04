c81cccee9029de8138969026418a2f4a59bed817
Gustavo-HA <gustavoha40@gmail.com>
2025-07-04 16:49:46 -0600

    model selection

--- lap/modeling/model_selection.py
+++ lap/modeling/model_selection.py
@@ -6,14 +6,18 @@ from sklearn.ensemble import (
     RandomForestClassifier,
     GradientBoostingClassifier
 )
+from sklearn.svm import SVC
+from xgboost import XGBClassifier
 from sklearn.metrics import (
     accuracy_score,
     precision_score,
     recall_score,
     f1_score
 )
-from prefect import flow, task
+from prefect import flow, task, get_run_logger
 import mlflow
+from mlflow.models import infer_signature
+from mlflow.client import MlflowClient
 
 from loguru import logger
 from tqdm import tqdm
@@ -28,6 +32,17 @@ from lap.config import (
 
 app = typer.Typer()
 
+def get_metrics(y_true, y_pred, training: bool = True):
+    """Calculate and return the evaluation metrics."""
+    prefix = "tr_" if training else "test_"
+    accuracy = accuracy_score(y_true, y_pred)
+    precision = precision_score(y_true, y_pred)
+    recall = recall_score(y_true, y_pred)
+    f1 = f1_score(y_true, y_pred)
+    return {prefix+"accuracy": accuracy,
+            prefix+"precision": precision,
+            prefix+"recall": recall,
+            prefix+"f1": f1}
 
 @task(name="train_model",
 )
@@ -50,35 +65,68 @@ def train_model(
     models = {
         "Logistic Regression": LogisticRegression(random_state=42),
         "RandomForest Classifier": RandomForestClassifier(random_state=42),
-        "Gradient Boosting Classifier": GradientBoostingClassifier(random_state=42)
+        "Gradient Boosting Classifier": GradientBoostingClassifier(random_state=42),
+        "Support Vector Classifier": SVC(random_state=42),
+        "XGBoost Classifier": XGBClassifier(random_state=42),
     }
     
-    for model_name, model in models.items():
+    for model_name, model in tqdm(models.items(), desc="Training models"):
         with mlflow.start_run(run_name=model_name):
             logger.info(f"Training default {model_name}...")
             
             mlflow.set_tag("model_name", model_name)
             
-            model.fit(X_train, y_train)
-            
-            y_pred = model.predict(X_test)
-            
-            accuracy = accuracy_score(y_test, y_pred)
-            precision = precision_score(y_test, y_pred)
-            recall = recall_score(y_test, y_pred)
-            f1 = f1_score(y_test, y_pred)
+            model.fit(X_train, y_train.values.ravel())
             
             mlflow.log_params(model.get_params())
             
-            mlflow.log_metric("accuracy", accuracy)
-            mlflow.log_metric("precision", precision)
-            mlflow.log_metric("recall", recall)
-            mlflow.log_metric("f1_score", f1)
+            y_tr_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
             
-            mlflow.sklearn.log_model(model, "model")
+            signature = infer_signature(X_train, y_tr_pred)
+            
+            tr_metrics = get_metrics(y_train.values.ravel(), y_tr_pred, training=True)
+            test_metrics = get_metrics(y_test.values.ravel(), y_test_pred, training=False)
+            
+            # Log both training and test metrics
+            for metric, value in tr_metrics.items():
+                mlflow.log_metric(metric, value)
+            for metric, value in test_metrics.items():
+                mlflow.log_metric(metric, value)
+            
+            mlflow.sklearn.log_model(sk_model=model, 
+                                     name="model",
+                                     signature=signature)
             
             logger.success(f"{model_name} trained and logged successfully.")
-            
+        
+    select_best_model()
+
+@task(
+    name="Select Best Model"
+)
+def select_best_model():
+    """Select the best model based on the logged metrics."""
+    logger.info("Selecting the best model based on logged metrics...")
+    
+    client = MlflowClient(tracking_uri=MLFLOW_TRACKING_URI)
+    runs = client.search_runs(
+        experiment_ids=mlflow.get_experiment_by_name(
+            MLFLOW_EXPERIMENT_NAME
+            ).experiment_id,
+        filter_string="",
+        run_view_type=mlflow.entities.ViewType.ACTIVE_ONLY,
+        max_results=1000,
+        order_by=["metrics.test_f1 DESC"]
+    )
+    
+    best_run = runs[0] if runs else None
+    
+    if best_run:
+        logger.success(f"Best model found: {best_run.data.tags['model_name']} with test F1 score: {best_run.data.metrics['test_f1']}")
+        logger.info(f"Run ID: {best_run.info.run_id}")
+    else:
+        logger.warning("No runs found.")
 
 @flow(
     name="Model Training",
@@ -87,6 +135,10 @@ def training_flow(
     features_path: Path = PROCESSED_DATA_DIR / "features.csv",
     labels_path: Path = PROCESSED_DATA_DIR / "labels.csv",
 ):
+    # Configure Loguru to use Prefect's logger
+    logger.remove()
+    logger.add(sink=get_run_logger().info, format="{message}")
+    
     mlflow.set_tracking_uri(MLFLOW_TRACKING_URI)
     mlflow.set_experiment(MLFLOW_EXPERIMENT_NAME)
     
