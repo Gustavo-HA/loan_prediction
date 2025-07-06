6a5d5c2c9dc76194a377c670d8ec4d63ca08175d
Gustavo-HA <gustavoha40@gmail.com>
2025-07-06 17:18:04 -0600

    main pipeline for processing data and re-training model

--- lap/main_flow.py
+++ lap/main_flow.py
@@ -5,20 +5,19 @@ from lap.modeling.hp_optim import hp_optim_flow
 from lap.modeling.train import final_training_flow
 
 
-@flow(name="Main Loan Prediction Pipeline")
-def main_pipeline_flow():
+@flow(name="Main Pipeline for Processing Data and Retraining Model")
+def main():
     """
     The main production pipeline that runs all steps in order:
     1. Data Preprocessing
     2. Hyperparameter Optimization
     3. Final Model Training
     """
-    # The `wait_for` parameter ensures Prefect executes these flows in the
-    # correct order.
-    data_run = data_preprocessing_flow.submit()
-    hp_run = hp_optim_flow.submit(wait_for=[data_run])
-    final_training_flow.submit(wait_for=[hp_run])
+    # Call the flows directly. `wait_for` ensures they run in the correct order.
+    data_run = data_preprocessing_flow()
+    hp_run = hp_optim_flow(wait_for=[data_run])
+    final_training_flow(wait_for=[hp_run])
 
 
 if __name__ == "__main__":
-    main_pipeline_flow()
+    main()
