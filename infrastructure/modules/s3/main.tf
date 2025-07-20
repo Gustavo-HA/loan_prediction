1ef855847bf004a55aab903a2692458306c2e74f
Gustavo-HA <gustavoha40@gmail.com>
2025-07-19 18:26:30 -0600

    Update S3 bucket configuration and staging variables for event streams and model references

--- infrastructure/modules/s3/main.tf
+++ infrastructure/modules/s3/main.tf
@@ -1,6 +1,9 @@
 resource "aws_s3_bucket" "s3_bucket" {
     bucket = var.bucket_name
     acl = "private"
+
+    # Allows Terraform to delete the bucket even if it contains objects
+    force_destroy = true
 }
 
 output "bucket_name" {
